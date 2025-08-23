896f9b0947d5eb13e023d3917c015e9902837b95
anik05169 <anikmain05169@gmail.com>
2025-08-23 12:09:22 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -23,18 +23,31 @@ def preprocess_image(image: Image.Image, target_size=(224, 224)):
 
 
 def make_gradcam_heatmap(img_array, model, last_conv_layer_name="block5_conv3"):
+    
     grad_model = tf.keras.models.Model(
-        [model.inputs], [model.get_layer(last_conv_layer_name).output, model.output]
+        [model.inputs],
+        [model.get_layer(last_conv_layer_name).output, model.output]
     )
+
     with tf.GradientTape() as tape:
         conv_outputs, predictions = grad_model(img_array)
-        class_channel = tf.reduce_max(predictions, axis=1) 
-    grads = tape.gradient(class_channel, conv_outputs)
+        pred_index = tf.argmax(predictions[0])  
+        class_channel = predictions[:, pred_index]
+
+    
+    grads = tape.gradient(class_channel, conv_outputs)    
     pooled_grads = tf.reduce_mean(grads, axis=(0, 1, 2))
+
     conv_outputs = conv_outputs[0]
+    
     heatmap = conv_outputs @ pooled_grads[..., tf.newaxis]
     heatmap = tf.squeeze(heatmap)
-    return np.maximum(heatmap, 0) / np.max(heatmap)
+
+    heatmap = np.maximum(heatmap, 0)
+    if np.max(heatmap) != 0:
+        heatmap /= np.max(heatmap)
+
+    return heatmap.numpy()
 
 
 def overlay_heatmap(original_img, heatmap, alpha=0.4):
